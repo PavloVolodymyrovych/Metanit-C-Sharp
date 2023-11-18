3ae4b506ee61e09f474757dc43c39e097c060e53
PM-DE✅ <126395347+PavloVolodymyrovych@users.noreply.github.com>
2023-11-18 13:05:46 +0200

    Create Cycles.cs

---
+++ Cycles.cs
@@ -0,0 +1,54 @@
+// https://metanit.com/sharp/practice/1.2.php
+
+//PART #1
+//Console.Write("Enter deposit amount: ");
+//decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+//for (int i = 0; i < 7; i++)
+//{
+//    depositAmount += depositAmount * 0.07m;
+//}
+//Console.WriteLine($"Amount per month: {depositAmount}");
+
+
+//PART #2
+//Console.Write("Enter deposit amount: ");
+//decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+//int i = 0;
+//while(i < 7)
+//{
+//    depositAmount += depositAmount * 0.07m;
+//    i++;
+//}
+//Console.WriteLine($"Amount per month: {depositAmount}");
+
+
+//PART #3
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine("");
+//    for (int j = 1; j <= 10; j++)
+//        Console.Write($"{i} * {j} = {i*j}\n");
+//}
+
+
+//PART #4
+//Console.Write("Введите число А которое находится в диапазоне от 0 до 10: ");
+//double numberA = Convert.ToDouble(Console.ReadLine());
+//while (true) {
+//    if(numberA >= 0 && numberA <= 10) { break; }
+//    else
+//    {
+//        Console.WriteLine("Введено недопустимое чило. Повторите.");
+//    }
+//}
+//Console.Write("Введите число В которое находится в диапазоне от 0 до 10: ");
+//double numberB = Convert.ToDouble(Console.ReadLine());
+//while (true)
+//{
+//    if (numberB >= 0 && numberB <= 10) { break; }
+//    else
+//    {
+//        Console.WriteLine("Введено недопустимое чило. Повторите.");
+//    }
+//}
+//Console.WriteLine($"Результат произведения двух чисел равен: {numberA * numberB}");
